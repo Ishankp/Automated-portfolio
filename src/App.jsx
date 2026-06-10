cdde00e7e7fc6aa1fcfd13db6c5682ca279baa1a
Ishan Patel <ishan03kp@gmail.com>
2026-06-10 12:53:34 -0400

    impletmented a navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,13 @@ import info from '../info.json';
 import "./App.css";
 import Header from './components/header.jsx';
 import Profile from './components/profile.jsx';
+import Navbar from './components/navbar.jsx';
 
 function App() {
   return (
     <div className="main-container">
+      {/* Navbar section */}
+      <Navbar />
       {/* Header section */}
       <Header name={info.name} title={info.title} />
 
